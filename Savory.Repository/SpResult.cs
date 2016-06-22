d88a9e89960b30acb1aab6aba724f2a645d9519f
HarrisZhang <hc_zhang@Ctrip.com>
2016-06-22 12:17:33 +0800

    update

---
+++ Savory.Repository/SpResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savory.Repository
+{
+    /// <summary>
+    /// 返回码
+    /// </summary>
+    public enum SpResult
+    {
+        /// <summary>
+        /// 失败
+        /// </summary>
+        Failed = 0,
+
+        /// <summary>
+        /// 成功
+        /// </summary>
+        Successful = 1,
+
+        /// <summary>
+        /// 登录成功
+        /// </summary>
+        EmailSignin_Successful = 3,
+
+        /// <summary>
+        /// 邮箱还没有被激活
+        /// </summary>
+        EmailSignin_EmailNotActive = 4,
+
+        /// <summary>
+        /// 用户名或密码不正确
+        /// </summary>
+        EmailSignin_Failed = 5
+    }
+}

325319b142e86bf0325c5200db55bd3f33caf1b2
HarrisZhang <hc_zhang@Ctrip.com>
2016-07-05 11:05:09 +0800

    update

--- Savory.Repository/SpNameConst_Favorite.cs
+++ Savory.Repository/SpNameConst_Favorite.cs
@@ -4,5 +4,7 @@
     {
 
         public const string Sp_Favorite_GetFavorite = "Sp_Favorite_GetFavorite";
+
+        public const string Sp_Favorite_GetFavoriteList = "Sp_Favorite_GetFavoriteList";
     }
 }
---
+++ Savory.Repository/SqlProvider.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+
+namespace Savory.Repository
+{
+    /// <summary>
+    /// SqlConnectionProvider
+    /// </summary>
+    public static class SqlProvider
+    {
+        /// <summary>
+        /// Gets an opened SqlConnection instance.
+        /// </summary>
+        /// <param name="connectioName">Name of the connection</param>
+        /// <returns></returns>
+        public static SqlConnection GetSqlConnection(string connectioName)
+        {
+            var connString = ConfigurationManager.ConnectionStrings[connectioName].ConnectionString;
+
+            SqlConnection sqlConn = new SqlConnection(connString);
+
+            sqlConn.Open();
+
+            return sqlConn;
+        }
+
+        /// <summary>
+        /// Gets an SqlCommand instance from an opened SqlConnection instance for sp.
+        /// </summary>
+        /// <param name="storedProcedure">Name of a stored procedure.</param>
+        /// <param name="sqlConn">An opened SqlConnection instance.</param>
+        /// <returns></returns>
+        public static SqlCommand GetSqlCommandForSp(string storedProcedure, SqlConnection sqlConn)
+        {
+            SqlCommand sqlCmd = new SqlCommand();
+
+            sqlCmd.Connection = sqlConn;
+            sqlCmd.CommandText = storedProcedure;
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+
+            return sqlCmd;
+        }
+
+        /// <summary>
+        /// Gets an SqlCommand instance from an opened SqlConnection instance for text command.
+        /// </summary>
+        /// <param name="cmdText">Query or commands</param>
+        /// <param name="sqlConn">An opened SqlConnection instance.</param>
+        /// <returns></returns>
+        public static SqlCommand GetSqlCommandForText(string cmdText, SqlConnection sqlConn)
+        {
+            SqlCommand sqlCmd = new SqlCommand();
+
+            sqlCmd.Connection = sqlConn;
+            sqlCmd.CommandText = cmdText;
+
+            return sqlCmd;
+        }
+
+        /// <summary>
+        /// 获取连接字符串
+        /// </summary>
+        /// <returns></returns>
+        public static string GetConnString()
+        {
+            string connName = string.Format("SavoryMaster_{0}", Environment.MachineName);
+
+            var connString = ConfigurationManager.ConnectionStrings[connName].ConnectionString;
+
+            return connString;
+        }
+
+        /// <summary>
+        /// 获取连接字符串
+        /// </summary>
+        /// <returns></returns>
+        public static string GetConnName()
+        {
+            return string.Format("SavoryMaster_{0}", Environment.MachineName);
+        }
+    }
+}

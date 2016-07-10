354cc661706de6c7cef96b30b7fb256dd5774e1e
HarrisZhang <harriszhang@live.cn>
2016-07-10 15:10:09 +0800

    update

--- Savory.Repository/SpNameConst.cs
+++ Savory.Repository/SpNameConst.cs
@@ -57,6 +57,10 @@
 
         public const string Sp_ValidateAntiCsrf = "Sp_ValidateAntiCsrf";
 
-        public const string Sp_Master_UploadImage = "Sp_Master_UploadImage";
+        public const string Sp_Insert_SavoryImage = "Sp_Insert_SavoryImage";
+
+        public const string Sp_Insert_LocalImage = "Sp_Insert_LocalImage";
+
+        public const string Sp_Insert_QiniuImage = "Sp_Insert_QiniuImage";
     }
 }

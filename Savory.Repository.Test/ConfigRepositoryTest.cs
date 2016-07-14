e69aafce0dfedecba5f3f026298a65aa1964f6cf
HarrisZhang <hc_zhang@Ctrip.com>
2016-07-14 12:53:09 +0800

    update

---
+++ Savory.Repository.Test/ConfigRepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Savory.Repository.Test
+{
+    [TestClass]
+    public class ConfigRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var list = ConfigRepository.GetConfigEntityList();
+
+            Assert.IsNotNull(list);
+
+            Assert.AreNotEqual(0, list.Count);
+        }
+    }
+}
